8f271dbd3a8a6886fc1a10f3f88194d9c526407e
Rafael Fuidio <rafafuidio5234@gmail.com>
2025-12-01 19:22:28 -0300

    commit

---
+++ src/main/java/Claims/claims/Claims.java
@@ -0,0 +1,17 @@
+package Claims.claims;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Claims extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
